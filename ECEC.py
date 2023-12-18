8f09fb0839be97fbaf583e0fc024f6605b11402a
rafael pereira <rafael.pereira.estal@gmail.com>
2023-12-18 20:40:09 +0100

    contiene lista de subprogramas a ejecutar

--- ECEC.py
+++ ECEC.py
@@ -19,176 +19,219 @@ os.chdir(dire)
 print('Directorio de trabajo: ',os.getcwd())
 
 
-listScripts=["Diputados11.py","funciones.py","Diputados2.py"]
+# +
+###################################################################
+# -
+
+#PARTE I: importación de datos del Ministerio del interior
+listScripts=["Diputados11.py"]
 for script in listScripts:
     with open(script,encoding="utf-8") as f:
         contents = f.read()
     exec(contents)
 
+# +
+###################################################################
+# -
+
+#PARTE II: funciones de análisis y su importacoón para comprobaciones
+listScripts=["funciones.py","Diputados2.py"]
+for script in listScripts:
+    with open(script,encoding="utf-8") as f:
+        contents = f.read()
+    exec(contents)
+
+# +
+###################################################################
+# -
+
+# SEGUNDA FASE.PARTE I: IMPORTACIÓN DE NUEVOS DATOS
 listScripts=["Diputados31.py"]
 for script in listScripts:
     with open(script,encoding="utf-8") as f:
         contents = f.read()
     exec(contents)
 
+# +
+###################################################################
+# -
+
+#PARTE II: INTRODUCIR NUEVOS GRUPOS
 listScripts=["Diputados32.py"]
 for script in listScripts:
     with open(script,encoding="utf-8") as f:
         contents = f.read()
     exec(contents)
 
+# +
+#################################################################
+# -
+
+#PARTE III: ELIMINAR DE NUEVO CANDIDATURAS <barrera
+#comienza copiando df3=df1.copy()
 listScripts=["Diputados33.py"]
 for script in listScripts:
     with open(script,encoding="utf-8") as f:
         contents = f.read()
     exec(contents)
 
-df3.loc[30]['PARTIDOS>']
+estructura(df3)
 
 # +
+##############################################################
+# -
+
 #PARTE IV: TABLAS d'HONDT
+#No modifica df3
+listScripts=["Diputados34.py"]
+for script in listScripts:
+    with open(script,encoding="utf-8") as f:
+        contents = f.read()
+    exec(contents)
+
+# +
+###############################################################
 # -
 
-list1 = [int(x) for x in D]
-list1.sort()
-claves = [str(x) for x in list1]
+# PARTE V: COMPROBAR SI HAY EMPATES
+#No modifica df3
+listScripts=["Diputados35.py"]
+for script in listScripts:
+    with open(script,encoding="utf-8") as f:
+        contents = f.read()
+    exec(contents)
 
-#lista de partidos por provincia que superan la barrera
-no_nulos=[[] for j in range(N_PROV)]
-for j in range(N_PROV):
-    for x in claves:
-        if(df3.loc[j][x])!=0:
-            no_nulos[j].append(x)    
+# +
+################################################################
+# -
 
-#partidos que superan el mínimo de votos (barrera) en provincia 30
-no_nulos[0]
+#PARTE VI: SI HAY EMPATES
+#hago df4=df3.copy() a medio programa
+#df4 contiene escaños asignados cuando no hay sorteo
+#df5 contiene escaños asignados cuando hay sorteo
+listScripts=["Diputados36.py"]
+for script in listScripts:
+    with open(script,encoding="utf-8") as f:
+        contents = f.read()
+    exec(contents)
 
-#creo un diccionario Koef vacío salvo par partidos para guardar
- #las tablas d'Hondt para cada provincia
-Koef=[[] for j in range(N_PROV)]
-Dict={}
-for j in range(N_PROV):
-    coef=[i for i in range(1,int(df3.loc[j]['DIPUTADOS']))]
-    for x in no_nulos[j]:
-        Dict['partido']=x
-        for c in coef:
-            Dict[c]=0
-        a=Dict
-        #print(j,a)
-        Koef[j].append(a.copy())
+# +
+##################################################################
+# -
 
-Koef[0]
+#PARTE VII: ASIGNACIÓN DE ESCAÑOS DEFINITIVA
+#recibe df5 y lo transforma en df6,df7,df71,DF71 y sale DF72
+listScripts=["Diputados37.py"]
+for script in listScripts:
+    with open(script,encoding="utf-8") as f:
+        contents = f.read()
+    exec(contents)
 
-#genero DataFrames para tablas d'Hondt a partir de los diccionarios
-for j in range(N_PROV):    
-    K[j]=pd.DataFrame(Koef[j])  
-    K[j].insert(loc=0, column='provincia', value=0, allow_duplicates = False)
+# +
+#############################################################3
+# -
 
-dHondt=[[] for i in range(N_PROV)]
-for i in range(len(df3)):
-    dHondt[i]=pd.DataFrame(K[i])
-    dHondt[i].index=list(dHondt[i][:]['partido'])
-
-
-for i in range(len(df3)):
-     for k in list(dHondt[i][:]['partido']):
-        dHondt[i].loc[k,'provincia']=i
+dipugrupos
 
 for i in range(len(df3)):
-    
-    for k in list(dHondt[i][:]['partido']):
-            
-        for l in range (1,int(df3.loc[i]['DIPUTADOS'])+1):
-            a=float(df3.loc[i][k])/float(l)
-            dHondt[i].loc[k,l]=a
+    print('-PROVINCIA',i,'(',df1.loc[i]['PROVINCIA'].strip(),')','\n --DIPUTADOS:')
+    print('   DERECHA',DF72.loc[i]['DIPDERECHA'])
+    print('   IZQUIERDA',DF72.loc[i]['DIPIZQUIERDA'])
+    print('   CENTRO',DF72.loc[i]['DIPCENTRO'])
+    print('   NACIONALISTAS',DF72.loc[i]['DIPNACIONALISTAS'])
+    print('   OTROS',DF72.loc[i]['DIPOTROS'])
+    print('  --TOTAL',DF72.loc[i][dipugrupos].sum(),'DIPUTADOS')
+
+print("---------------------------------------------------",
+     "---------------------------------------------------",
+     "TERMINADO:",guion+".py")
+
+# +
+##############################################################
+# -
+
+#PARTE VIII: ARCHIVO DE SALIDA EXCEL
+#recibe df5 y lo transforma en df6,df7,df71,DF71 y sale DF72
+listScripts=["Diputados38.py"]
+for script in listScripts:
+    with open(script,encoding="utf-8") as f:
+        contents = f.read()
 
 
 # +
-lista_votos=[[] for i in range(N_PROV)]
-for i in range(N_PROV):
-    N_DIP=list(dHondt[i].keys())[2:]
-    PARTS=list(dHondt[i].index)
-    lista_votos1[i]=[]
-    for j in PARTS:
-        #print('provincia',i,list(dHondt[i].loc[j][2:]))
-        lista_votos[i]=lista_votos[i]+list(dHondt[i].loc[j][2:])
-        lista_votos[i].sort(reverse=True)
-       
-      
+###################################################################
+
+# +
+#DIPUTADOS38: PARTE VIII: ARCHIVO DE SALIDA EXCEL
 # -
 
-#Exporto d'Hondt 
-F=input("¿DESEA EXPORTAR LAS TABLAS d'HONDT? (Y/N)")
+guion="Diputados38"
+
+#salida con ceros de datos MinInt
+output1=resultados.copy()
+
+estructura(output1)
+
+list(output1.loc[0].keys())[dipugrupos][9:]
+
+A=[]
+M1=[i for i in range(pos(df3,'DIPUTADOS')+1,pos(df3,'DDERECHA'))]
+M=list(df3.loc[0][M1].keys())
+for y in M:
+    if (df3[y] == 0).all():
+        A.append(y)
+
+common=set(A).intersection(set(output1.loc[0].keys()))
+#salida sin ceros
+output2=output1.drop(columns=list(common),axis=1)
+
+#salida con ceros de asignaciones
+output3=pd.concat([DF71[VV0],DF71[VV1],DF71[VV2],DF71[VV7],DF71[VV8]],axis=1)
+
+common=set(A).intersection(set(output3.loc[0].keys()))
+#salida sin ceros de asignaciones
+output4=output3.drop(columns=list(common),axis=1)
+
+#salida de >barrera con ceros
+output5=pd.concat([DF71[VV0],DF71[VV5],DF71[VV6],DF71[VV7],DF71[VV8]],axis=1)
+
+common=set(A).intersection(set(output5.loc[0].keys()))
+#salida sin ceros de >barrera
+output6=output5.drop(columns=list(common),axis=1)
+
+# +
+
+F=input("¿DESEA EXPORTAR LOS RESULTADOS? (Y/N)\n")
 if F=='Y' or F=='Y'.lower():
-    for i in range(N_PROV):
-        A=dHondt[i] 
-        B=pd.DataFrame(lista_votos[i])
-        B[B>0].dropna(axis=0, how='all') 
-        Name='dHondt'+str(i)
+    Res=input("¿Qué nombre desea concatenar con 'Resultados'\n")
+    Name='Resultados'+str(Res)
+    G=input("¿DESEA ELIMINAR VALORES NULOS DE VOTOS?\n\
+    (no es conveniente si se desea comparación con resultados iniciales\n(Y/N)\n") 
+    if G=='Y' or G=='Y'.lower():
         writer = pd.ExcelWriter(Name+'.xlsx')
-        A.to_excel(writer,'dHondt')
-        B.to_excel(writer,'lista_votos')
+        results=output2
+        results.to_excel(writer,sheet_name='DatosRealesMint')
+        results=output4
+        results.to_excel(writer,sheet_name='VotosReasignados')
+        results=output6
+        results.to_excel(writer,sheet_name='Datos>barrera')
         writer.close()
-
-# +
-#DIPUTADOS34: PARTE IV: TABLAS d'HONDT
+    if G=='N' or G=='N'.lower():
+        writer = pd.ExcelWriter(Name+'.xlsx')
+        results=output1
+        results.to_excel(writer,sheet_name='DatosRealesMint')
+        results=output3
+        results.to_excel(writer,sheet_name='VotosReasignados')
+        results=output5
+        results.to_excel(writer,sheet_name='Datos>barrera')
+        writer.close()
 # -
 
-p=[[] for i in range(N_PROV)]
-for i in range(N_PROV):
-    c=df3.loc[j,ll]
-    p[i]=[]
-    
-    for k in range (1,int(df3.loc[j]['DIPUTADOS'])+1):
-        part_voto=c/k
-        p[i].append(part_voto)
-#p[I][J] es una lista de dimensión N_PROVINCIAS y donde cada elemento de la lista es 
-#otra lista de longitud N_PARTIDOSxDIPUTADOS cuyo contenido es el número de votos de 
-#cada partido dividido por 1,2,...DIPUTADOS.
 
-# +
-import numpy as np
-q=[[] for i in range(N_PROV)]
-r=[[] for i in range(N_PROV)]
-dHondt=[[] for i in range(N_PROV)]
-for i in range(N_PROV):
-    r[i]=[]
-    for j in range(int(df3.loc[i]['DIPUTADOS'])):
-        A=p[i][j]
-        r[i].append(A.values)
-        q[i]=np.array((r[i])).T
-        dHondt[i]=pd.DataFrame(q[i])
-        
-#dHondt es un array que representa la tabla d'Hondt  
-# -
+print("---------------------------------------------------",
+     "---------------------------------------------------",
+     "TERMINADO:",guion+".py")
 
-#lista_votos[I] es una lista de longitud N_PROVINCIAS que tiene los valores ordenados de la 
-#tabla d'Hondt de cada provincia. Si no existen valores duplicados con lista_votos[I][DIPUTADOS-1]
-#en lista_votos[I][>=DIPUTADOS] se eligen los primeros términos hasta DIPUTADOS y se asignan los escaños
-#según las veces que aparezca cada partido. Si existen duplicados, se seleccionan aquellos partidos
-#que tienen el mismo valor del índice que lista_votos[I][DIPUTADOS-1] y, enre ellos, se eligen al azar
-#las candidaturas que deben completar el número de escaños. p.e. si hay DIPUTADOS=6 y 
-#lista_votos[I][4]=lista_votos[I][5]=lista_votos[I][6]=lista_votos[I][7]=1000 elegiremos aleatoriamente
-#dos de las cuatro empatadas para los dos escaños vacantes.
-lista_votos=[[] for i in range(N_PROV)]
-for i in range(N_PROV):
-    lista_votos[i]=[]
-    for j in range(int(df3.loc[i]['DIPUTADOS'])):
-        lista_votos[i]=lista_votos[i]+list(p[i][j])
-        
-        lista_votos[i].sort(reverse=True)
 
-#Exporto d'Hondt 
-F=input("¿DESEA EXPORTAR LAS TABLAS d'HONDT? (Y/N)")
-if F=='Y' or F=='Y'.lower():
-    for i in range(N_PROV):
-        A=dHondt[i][dHondt[i]>0].dropna(axis=0, how='all') 
-        B=pd.DataFrame(lista_votos[i])
-        B[B>0].dropna(axis=0, how='all') 
-        Name='dHondt'+str(i)
-        writer = pd.ExcelWriter(Name+'.xlsx')
-        A.to_excel(writer,'dHondt')
-        B.to_excel(writer,'lista_votos')
-        writer.close()
 
 
